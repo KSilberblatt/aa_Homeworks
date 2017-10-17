923b6f4c49138a1736a976ebce6cc47884d553a8
Kevin Silberblatt <ksilberblatt@cox.net>
2017-10-16 22:44:29 -0700

    Finish plays.rb exercise

--- W3D2/skeleton/plays.rb
+++ W3D2/skeleton/plays.rb
@@ -48,4 +48,84 @@ class Play
         id = ?
     SQL
   end
+
+  def Play::find_by_title(title)
+    PlayDBConnection.instance.execute(<<-SQL, title)
+      SELECT *
+      FROM plays
+      WHERE
+        title = ?
+    SQL
+  end
+
+  def Play::find_by_playwright(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT *
+      FROM plays
+      JOIN playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name = ?
+    SQL
+  end
+end
+
+class Playwright
+
+  def Playwright::all
+    PlayDBConnection.instance.execute(<<-SQL)
+      SELECT *
+      FROM playwrights
+    SQL
+  end
+
+  def Playwright::find_by_name(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT *
+      WHERE
+        playwrights.name = ?
+    SQL
+  end
+
+  def initialize(options)
+    new(options)
+  end
+
+  def new(options) #(this is the initialize method)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def self::get_plays(name) #(returns all plays written by playwright)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT plays.title, playwrights.name
+      FROM plays
+      JOIN playwrights ON plays.playwright_id = playwrights.id
+      WHERE
+        playwrights.name = ?
+    SQL
+  end
 end
